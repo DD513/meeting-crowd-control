c922aaafe376cb438b8a68b0def3d1fc1e137acd
DD <50895419+DD513@users.noreply.github.com>
2022-07-22 11:56:36 +0800

    fix: fix image box

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -134,6 +134,7 @@ function Index() {
             <Col xs={24} md={13}>
               <div className="imgBlock">
                 <Image
+                  className="pythonImage"
                   src={
                     pythonData.data[0] !== undefined
                       ? `${config.api}/${pythonData.data[0].imageUrl}`
